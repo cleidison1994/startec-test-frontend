8056d09ae0372784d562bbfa6ddf92875a8396ac
cleidison <cleidisonalmei@hotmail.com>
2020-01-27 21:04:33 -0300

    adiciona dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 import axios from 'axios';
 
 const api  = axios.create({
-    baseURL:process.env.REACT_APP_API_UR
+    baseURL:process.env.REACT_APP_API_URL
 })
 export default api;
